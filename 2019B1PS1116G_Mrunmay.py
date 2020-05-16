17af736ac669155f621e0d141e7c103dfda0f566
Mrunmay <mrunmayshelar@gmail.com>
2020-05-16 22:51:27 +0530

    assignment

--- 2019B1PS1116G_Mrunmay.py
+++ 2019B1PS1116G_Mrunmay.py
@@ -3,16 +3,11 @@ import matplotlib as plt
 from PIL import Image
 path = input("Specify img path:")
 img = Image.open(path)
-
 nimg = np.array(img)
 invarr = np.invert(nimg)
-
 invimg = Image.fromarray(invarr)
-
 invpath = input("Specify inverted path:")
-
 invimg.save(invpath)
-
 rgbwts = np.array([0.2989, 0.5870, 0.1140])
 monoarr = np.dot(nimg,rgbwts)
 monoimg = Image.fromarray(monoarr)
