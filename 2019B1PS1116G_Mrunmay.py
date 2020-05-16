3998d4d4f517a90464c1b84e7160ac94307fcc6a
Mrunmay <mrunmayshelar@gmail.com>
2020-05-16 22:35:47 +0530

    Assignment for Week 1

---
+++ 2019B1PS1116G_Mrunmay.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib as plt
+from PIL import Image
+path = input("Specify img path:")
+img = Image.open(path)
+
+nimg = np.array(img)
+invarr = np.invert(nimg)
+
+invimg = Image.fromarray(invarr)
+
+invpath = input("Specify inverted path:")
+
+invimg.save(invpath)
+
+rgbwts = np.array([0.2989, 0.5870, 0.1140])
+monoarr = np.dot(nimg,rgbwts)
+monoimg = Image.fromarray(monoarr)
+#monopath = input("Specify monochrome path:")
+#monoimg.save(monopath)
+monoimg.show()
+
